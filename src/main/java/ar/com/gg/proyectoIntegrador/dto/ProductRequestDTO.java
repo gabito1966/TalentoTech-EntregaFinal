655c3f01f2310b7d389e6dcdbcd2b8bd3fd688c3
Gabriel García <89267342+gabito1966@users.noreply.github.com>
2025-07-22 20:51:21 -0300

    Update ProductRequestDTO.java

--- src/main/java/ar/com/gg/proyectoIntegrador/dto/ProductRequestDTO.java
+++ src/main/java/ar/com/gg/proyectoIntegrador/dto/ProductRequestDTO.java
@@ -2,14 +2,32 @@ package ar.com.gg.proyectoIntegrador.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.*;
 
 @Getter
 @Setter
 public class ProductRequestDTO {
-    private Long id;
-    private String image;
+
+    // Atributo para el nombre del producto
+    @NotBlank(message = "El nombre no puede estar vacío.")
+    @Size(max = 100, message = "El nombre no puede tener más de 100 caracteres.")
     private String name;
+
+    // Atributo para la descripción del producto
+    @Size(max = 500, message = "La descripción no puede tener más de 500 caracteres.")
     private String description;
+
+    // Atributo para la ruta de la imagen
+    @Size(max = 255, message = "La ruta de la imagen no puede tener más de 255 caracteres.")
+    private String image;
+
+    // Atributo para el precio del producto
+    @NotNull(message = "El precio no puede ser nulo.")
+    @Positive(message = "El precio debe ser positivo.")
     private double price;
+
+    // Atributo para el stock del producto
+    @NotNull(message = "El stock no puede ser nulo.")
+    @Min(value = 0, message = "El stock no puede ser negativo.")
     private int stock;
 }
